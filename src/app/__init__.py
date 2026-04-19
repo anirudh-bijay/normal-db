88519df66f4f854da161673ba902e925d4775af9
anirudh-bijay <190929747+anirudh-bijay@users.noreply.github.com>
2026-04-19 20:02:35 +0530

    Make app installable

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3
-
-# import normaldb
 from flask import Flask, render_template, request, url_for
+
+import normaldb
 
 # TODO: Create a frontend that allows users to interact with the
 #       normaldb package.
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("./index.html")
+
 
 @app.route("/normalize", methods=["POST"])
 def normalize():
@@ -25,5 +26,7 @@ def normalize():
         print("Normalization error:", error)
         return {"error": "Failed to parse request."}, 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+def create_app():
+    app = Flask(__name__)
+    return app
